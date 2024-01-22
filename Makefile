235b95cc4d403080805f1348a7f473c7fb58e45e
miyake13000 <miyake2020@s.okayama-u.ac.jp>
2024-01-22 23:45:25 +0900

    Update plugin manager and method of installing tools

--- Makefile
+++ Makefile
@@ -1,63 +1,63 @@
 CWD = $(shell pwd)
 
+BINSTALL_INSTALL = curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
+BINSTALL_PATH = $(shell which cargo-binstall || echo ~/.cargo/bin/cargo-binstall)
+BINSTALL = $(BINSTALL_PATH) --no-confirm
+
 usage:
-	@echo "dotfiles for miyake13000 (https://github.com/miyake13000)"
-	@echo ""
-	@echo "This Makefile can setup/update below tools"
-	@echo "  zsh"
-	@echo "  nvim"
-	@echo "  tmux"
-	@echo "  anyenv (some languages' environment)"
-	@echo "You can setup above tools with below command"
-	@echo "  zsh    -> make setup-zsh"
-	@echo "  nvim   -> make setup-nvim"
-	@echo "  tmux   -> make setup-tmux"
-	@echo "  anyenv -> make setup-lang"
-	@echo "  above all -> setup-all"
-	@echo "You can also update them"
-	@echo "  zsh    -> make update-zsh"
-	@echo "  nvim   -> make update-nvim"
-	@echo "  tmux   -> make update-tmux"
-	@echo "  anyenv -> make update-lang"
-	@echo "  above all -> update-all"
+	@echo "Setup script for miyake13000's dotfiles' (https://github.com/miyake13000/dotfiles)"
+	@echo
+	@echo "This Makefile can setup/update some tools"
+	@echo "--------------------------------------------------------"
+	@echo "Execute 'make setup' to update / 'make update' to update"
+	@echo "--------------------------------------------------------"
+	@echo "Or you can setup/update each tools individually"
+	@echo "  zsh    (make setup-zsh  / update-zsh)"
+	@echo "  nvim   (make setup-nvim / update-nvim)"
+	@echo "  tmux   (make setup-tmux / no need to update)"
+	@echo "  anyenv (make setup-lang / update-lang)"
+	@echo "  git    (make setup-git  / update-git)"
+	@echo
 
 check-command:
-	@which git    > /dev/null || (echo "Install git with 'sudo apt install git'"    ; exit 1)
-	@which wget   > /dev/null || (echo "Install git with 'sudo apt install wget'"   ; exit 1)
-	@which curl   > /dev/null || (echo "Install git with 'sudo apt install curl'"   ; exit 1)
-	@which tar    > /dev/null || (echo "Install git with 'sudo apt install tar'"    ; exit 1)
-	@which gunzip > /dev/null || (echo "Install git with 'sudo apt install gunzip'" ; exit 1)
-	@which zsh    > /dev/null || (echo "Install git with 'sudo apt install zsh'"    ; exit 1)
+	@which git    > /dev/null || (echo "Install git"    ; exit 1)
+	@which curl   > /dev/null || (echo "Install curl"   ; exit 1)
+	@which tar    > /dev/null || (echo "Install tar"    ; exit 1)
+	@which gunzip > /dev/null || (echo "Install gunzip" ; exit 1)
+	@which zsh    > /dev/null || (echo "Install zsh"    ; exit 1)
+	@which tmux   > /dev/null || (echo "Install tmux"   ; exit 1)
+	@[ -e $(BINSTALL_PATH) ] > /dev/null || $(BINSTALL_INSTALL)
 
-setup-all: setup-zsh setup-nvim setup-tmux setup-git setup-lang
+setup: setup-zsh setup-nvim setup-tmux setup-git setup-lang
 
 setup-zsh: check-command
 	mkdir -p ~/.config
+	mkdir -p ~/.config/sheldon
 	mkdir -p ~/.local/bin
 	ln -sf $(CWD)/zsh/zshrc ~/.zshrc
 	ln -sf $(CWD)/zsh/starship.toml ~/.config/starship.toml
-	$(CWD)/scripts/install-tools.sh starship
-	$(CWD)/scripts/install-tools.sh fzf
+	ln -sf $(CWD)/zsh/plugins.toml ~/.config/sheldon/plugins.toml
+	$(BINSTALL) sheldon
+	$(BINSTALL) starship
+	$(BINSTALL) fzf
 
 setup-nvim: check-command
 	mkdir -p ~/.config/nvim
 	mkdir -p ~/.local/bin
 	mkdir -p ~/.local/share/nvim
 	ln -sf $(CWD)/vim/init.lua ~/.config/nvim/init.lua
-	ln -sf $(CWD)/vim/snip ~/.local/share/nvim/snip
+	ln -sf $(CWD)/vim/snip ~/.local/share/nvim/
 	ln -sf $(CWD)/scripts/nvim-update.sh ~/.local/bin/nvim-update
 	ln -sf $(CWD)/scripts/ggr.sh ~/.local/bin/ggr
 	$(CWD)/scripts/nvim-update.sh --force
-	$(CWD)/scripts/install-tools.sh bat
-	$(CWD)/scripts/install-tools.sh fd
-	$(CWD)/scripts/install-tools.sh rg
-	$(CWD)/scripts/install-tools.sh shellcheck
+	$(BINSTALL) bat
+	$(BINSTALL) fd-find
+	$(BINSTALL) ripgrep
 	$(CWD)/scripts/install-tools.sh tree-sitter
+	$(CWD)/scripts/install-tools.sh lazygit
 
 setup-tmux: check-command
-	$(CWD)/scripts/install-tools.sh tmux
 	ln -sf $(CWD)/tmux/tmux.conf ~/.tmux.conf
-
 
 setup-lang: check-command
 ifeq ("$(wildcard ~/.anyenv)", "")
@@ -70,24 +70,28 @@ endif
 	~/.anyenv/bin/anyenv install --force-init
 
 setup-git: check-command
-	mkdir -p ~/.local/bin
 	cat $(CWD)/git/gitconfig >> ~/.gitconfig
-	$(CWD)/scripts/install-tools.sh delta
+	$(BINSTALL) git-delta
 
-update-all: update-zsh update-nvim update-lang
+update: update-zsh update-nvim update-lang update-git
 
 update-zsh: check-command
-	$(CWD)/scripts/install-tools.sh starship
-	$(CWD)/scripts/install-tools.sh fzf
+	mkdir -p ~/.local/bin
+	$(BINSTALL) sheldon
+	$(BINSTALL) starship
+	$(BINSTALL) fzf
 
 update-nvim: check-command
+	mkdir -p ~/.local/bin
 	$(CWD)/scripts/nvim-update.sh --force
-	$(CWD)/scripts/install-tools.sh bat
-	$(CWD)/scripts/install-tools.sh fd
-	$(CWD)/scripts/install-tools.sh rg
-	$(CWD)/scripts/install-tools.sh shellcheck
+	$(BINSTALL) bat
+	$(BINSTALL) fd-find
+	$(BINSTALL) ripgrep
 	$(CWD)/scripts/install-tools.sh tree-sitter
 	$(CWD)/scripts/install-tools.sh lazygit
 
 update-lang:;	anyenv update
+
+update-git: check-command
+	$(BINSTALL) git-delta
 
--- scripts/install-tools.sh
+++ scripts/install-tools.sh
@@ -7,12 +7,6 @@ if [ "$1" = "" ]; then
     install-tools.sh <tool-name>
 
     tool-name:
-    bat           cat alternative
-    delta         diff alternative
-    fd            find alternative
-    rg            grep alternative
-    shellcheck    syntax parser for shell script
-    starship      shell prompt customizer
     tree-sitter   syntax parser
     lazygit       CUI git integrator
 
@@ -24,65 +18,9 @@ fi
 mkdir -p /tmp/install-tools/
 cd /tmp/install-tools
 
-if [ "$1" = "bat" ] || [ "$1" = "all" ]; then
-    bat_version=$(curl -Ls https://github.com/sharkdp/bat/releases/latest | grep "<title>"  | awk '{print $2}')
-    echo "Installing bat (${bat_version})..."
-    wget -q https://github.com/sharkdp/bat/releases/download/${bat_version}/bat-${bat_version}-x86_64-unknown-linux-musl.tar.gz > /dev/null
-    tar xzf bat-${bat_version}-x86_64-unknown-linux-musl.tar.gz > /dev/null
-    mv bat-${bat_version}-x86_64-unknown-linux-musl/bat ~/.local/bin/bat > /dev/null
-    echo "done"
-fi
-
-if [ "$1" = "delta" ] || [ "$1" = "all" ]; then
-    delta_version=$(curl -Ls https://github.com/dandavison/delta/releases/latest | grep "<title>"  | awk '{print $2}')
-    echo "Installing delta (${delta_version})..."
-    wget -q https://github.com/dandavison/delta/releases/download/${delta_version}/delta-${delta_version}-x86_64-unknown-linux-musl.tar.gz > /dev/null
-    tar xzf delta-${delta_version}-x86_64-unknown-linux-musl.tar.gz > /dev/null
-    mv delta-${delta_version}-x86_64-unknown-linux-musl/delta ~/.local/bin/delta > /dev/null
-    echo "done"
-fi
-
-if [ "$1" = "fd" ] || [ "$1" = "all" ]; then
-    fd_version=$(curl -Ls https://github.com/sharkdp/fd/releases/latest | grep "<title>"  | awk '{print $2}')
-    echo "Installing fd (${fd_version})..."
-    wget -q https://github.com/sharkdp/fd/releases/download/${fd_version}/fd-${fd_version}-x86_64-unknown-linux-musl.tar.gz > /dev/null
-    tar xzf fd-${fd_version}-x86_64-unknown-linux-musl.tar.gz > /dev/null
-    mv fd-${fd_version}-x86_64-unknown-linux-musl/fd ~/.local/bin/fd > /dev/null
-    echo "done"
-fi
-
-if [ "$1" = "rg" ] || [ "$1" = "all" ]; then
-    rg_version=$(curl -Ls https://github.com/BurntSushi/ripgrep/releases/latest | grep "<title>"  | awk '{print $2}')
-    echo "Installing rg (${rg_version})..."
-    wget -q https://github.com/BurntSushi/ripgrep/releases/download/${rg_version}/ripgrep-${rg_version}-x86_64-unknown-linux-musl.tar.gz > /dev/null
-    tar xzf ripgrep-${rg_version}-x86_64-unknown-linux-musl.tar.gz > /dev/null
-    mv ripgrep-${rg_version}-x86_64-unknown-linux-musl/rg ~/.local/bin/rg > /dev/null
-    echo "done"
-fi
-
-if [ "$1" = "shellcheck" ] || [ "$1" = "all" ]; then
-    redirect_url=$(curl -w "%{redirect_url}" -s -o /dev/null https://github.com/koalaman/shellcheck/releases/latest/)
-    version=$(echo $redirect_url | sed 's/\// /g' | awk '{print $NF}')
-    shellcheck_filename=$(curl -Ls https://github.com/koalaman/shellcheck/releases/expanded_assets/$version | grep "linux.x86_64" | grep "span" | sed -E 's/^ *<[^<]*>//' | sed -E 's/<.*>$//')
-    echo "Installing shellcheck..."
-    wget -q https://github.com/koalaman/shellcheck/releases/latest/download/${shellcheck_filename} > /dev/null
-    mkdir shellcheck > /dev/null
-    tar xf ${shellcheck_filename} -C shellcheck --strip-components 1 > /dev/null
-    mv shellcheck/shellcheck ~/.local/bin/shellcheck > /dev/null
-    echo "done"
-fi
-
-if [ "$1" = "starship" ] || [ "$1" = "all" ]; then
-    echo "Installing starship..."
-    wget -q https://github.com/starship/starship/releases/latest/download/starship-x86_64-unknown-linux-musl.tar.gz > /dev/null
-    tar xzf starship-x86_64-unknown-linux-musl.tar.gz > /dev/null
-    mv starship ~/.local/bin/starship > /dev/null
-    echo "done"
-fi
-
 if [ "$1" = "tree-sitter" ] || [ "$1" = "all" ]; then
     echo "Installing tree-sitter..."
-    wget -q https://github.com/tree-sitter/tree-sitter/releases/latest/download/tree-sitter-linux-x64.gz > /dev/null
+    curl -Ls https://github.com/tree-sitter/tree-sitter/releases/latest/download/tree-sitter-linux-x64.gz > tree-sitter-linux-x64.gz
     gunzip tree-sitter-linux-x64.gz > /dev/null
     chmod +x tree-sitter-linux-x64 > /dev/null
     mv tree-sitter-linux-x64 ~/.local/bin/tree-sitter > /dev/null
@@ -93,27 +31,9 @@ if [ "$1" = "lazygit" ] || [ "$1" = "all" ]; then
     lazygit_version=$(curl -Ls https://github.com/jesseduffield/lazygit/releases/latest | grep "<title>"  | awk '{print $2}')
     lazygit_version_stripped="${lazygit_version:1}"
     echo "Installing lazygit (${lazygit_version})..."
-    wget -q https://github.com/jesseduffield/lazygit/releases/download/${lazygit_version}/lazygit_${lazygit_version_stripped}_Linux_x86_64.tar.gz > /dev/null
-    tar xzf lazygit_${lazygit_version_stripped}_Linux_x86_64.tar.gz > /dev/null
+    curl -Ls https://github.com/jesseduffield/lazygit/releases/download/${lazygit_version}/lazygit_${lazygit_version_stripped}_Linux_x86_64.tar.gz > lazygit.tar.gz
+    tar xzf lazygit.tar.gz > /dev/null
     mv lazygit ~/.local/bin/lazygit > /dev/null
-    echo "done"
-fi
-
-if [ "$1" = "fzf" ] || [ "$1" = "all" ]; then
-    fzf_version=$(curl -Ls https://github.com/junegunn/fzf/releases/latest | grep "<title>"  | awk '{print $2}')
-    echo "Installing fzf (${fzf_version})..."
-    wget -q https://github.com/junegunn/fzf/releases/download/${fzf_version}/fzf-${fzf_version}-linux_amd64.tar.gz > /dev/null
-    tar xzf fzf-${fzf_version}-linux_amd64.tar.gz > /dev/null
-    mv fzf ~/.local/bin/fzf > /dev/null
-    echo "done"
-fi
-
-if [ "$1" = "tmux" ] || [ "$1" = "all" ]; then
-    tmux_version=$(curl -Ls https://github.com/nelsonenzo/tmux-appimage/releases/latest | grep "<title>" | awk '{print $3}')
-    echo "Installing tmux (${tmux_version})..."
-    wget -q https://github.com/nelsonenzo/tmux-appimage/releases/latest/download/tmux.appimage > /dev/null
-    chmod +x tmux.appimage
-    mv tmux.appimage ~/.local/bin/tmux > /dev/null
     echo "done"
 fi
 
